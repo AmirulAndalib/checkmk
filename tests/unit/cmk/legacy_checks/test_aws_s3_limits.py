3528641068bf2c8f29c0fbf9c6f9f73206949b80
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-07-02 21:04:40 +0200

    Legacy check migration: aws_s3_limits I
    Change-Id: I2b4a247fdb9595f203f16a18f0ba20529bcd8552

--- tests/unit/cmk/legacy_checks/test_aws_s3_limits.py
+++ tests/unit/cmk/legacy_checks/test_aws_s3_limits.py
@@ -4,14 +4,13 @@
 # conditions defined in the file COPYING, which is part of this source code package.
 
 # mypy: disable-error-code="misc"
-# mypy: disable-error-code="no-untyped-call"
 
 from collections.abc import Mapping, Sequence
 from typing import Any
 
 import pytest
 
-from cmk.agent_based.v2 import StringTable
+from cmk.agent_based.v2 import Metric, Result, Service, State, StringTable
 from cmk.legacy_checks.aws_s3_limits import check_aws_s3_limits, discover_aws_s3_limits
 from cmk.plugins.aws.lib import parse_aws_limits_generic
 
@@ -19,16 +18,13 @@ from cmk.plugins.aws.lib import parse_aws_limits_generic
 @pytest.mark.parametrize(
     "info, expected_discoveries",
     [
-        ([['[["buckets",', '"TITLE",', "10,", "1,", '"REGION"]]']], [("REGION", {})]),
+        ([['[["buckets",', '"TITLE",', "10,", "1,", '"REGION"]]']], [Service(item="REGION")]),
     ],
 )
-def test_discover_aws_s3_limits(
-    info: StringTable, expected_discoveries: Sequence[tuple[str, Mapping[str, Any]]]
-) -> None:
+def test_discover_aws_s3_limits(info: StringTable, expected_discoveries: Sequence[Service]) -> None:
     """Test discovery function for aws_s3_limits check."""
     parsed = parse_aws_limits_generic(info)
-    result = list(discover_aws_s3_limits(parsed))
-    assert sorted(result) == sorted(expected_discoveries)
+    assert list(discover_aws_s3_limits(parsed)) == list(expected_discoveries)
 
 
 @pytest.mark.parametrize(
@@ -39,16 +35,18 @@ def test_discover_aws_s3_limits(
             {"buckets": (None, 80.0, 90.0)},
             [['[["buckets",', '"TITLE",', "10,", "1,", '"REGION"]]']],
             [
-                (0, "No levels reached", [("aws_s3_buckets", 1)]),
-                (0, "\nTITLE: 1 (of max. 10)"),
+                Metric("aws_s3_buckets", 1.0),
+                Result(state=State.OK, notice="TITLE: 1 (of max. 10), 10.00%"),
             ],
         ),
     ],
 )
 def test_check_aws_s3_limits(
-    item: str, params: Mapping[str, Any], info: StringTable, expected_results: Sequence[Any]
+    item: str,
+    params: Mapping[str, Any],
+    info: StringTable,
+    expected_results: Sequence[Metric | Result],
 ) -> None:
     """Test check function for aws_s3_limits check."""
     parsed = parse_aws_limits_generic(info)
-    result = list(check_aws_s3_limits(item, params, parsed))
-    assert result == expected_results
+    assert list(check_aws_s3_limits(item, params, parsed)) == list(expected_results)
