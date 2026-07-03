b48645ed234d4afce58592fbfea24dbbca712434
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-07-03 14:17:06 +0200

    Add Werk #20119: Deprecate Python module livestatus, use cmk.livestatus_client
    CMK-36421
    CMK-36502
    
    Change-Id: I8208659df7c053429c1e681afb89133e6cdacb67

--- packages/cmk-livestatus-client/livestatus/__init__.py
+++ packages/cmk-livestatus-client/livestatus/__init__.py
@@ -2,7 +2,12 @@
 # Copyright (C) 2019 Checkmk GmbH - License: GNU General Public License v2
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
-"""MK Livestatus Python API"""
+"""MK Livestatus Python API
+
+Deprecated compatibility wrapper, announced in werk #20119.
+Use `cmk.livestatus_client` instead.
+This module will be removed in a future version of Checkmk (after 3.0).
+"""
 
 # TODO: The "bazel lint ..." calls for run_check_format() and run_check_ruff() don't agree on their findings. Why??
 from cmk.livestatus_client import *  # noqa: F403,RUF100
