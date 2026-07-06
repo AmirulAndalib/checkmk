ac567ccbc86bae80def793f4a872e52305950ccd
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-07-07 00:28:35 +0200

    Migrate super_server_solaris bakery rulespec to rulesets v1 API
    Replace old GUI API (DropdownChoice + rulespec_registry.register) with
    rulesets v1 Dictionary + SingleChoice form spec. Add migrate() function
    to convert old string values ("inetd"/"no_service") to new dict format
    {"deployment": "inetd"/"no_service"}. Update bakelet to handle new dict
    config format via _install_inetd_service() helper.
    
    Change-Id: Ic82903f2eab58d19c66086744e7b6e091d06a830

--- cmk/gui/plugins/legacy_bakery_rulesets/super_server_solaris.py
+++ cmk/gui/plugins/legacy_bakery_rulesets/super_server_solaris.py
@@ -3,21 +3,33 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
+from collections.abc import Mapping
 
-from cmk.gui.agent_bakery import RulespecGroupMonitoringAgentsLinuxUnixAgent
-from cmk.gui.i18n import _
-from cmk.gui.plugins.wato.utils import (
-    HostRulespec,
-    rulespec_registry,
+from cmk.rulesets.v1 import Help, Title
+from cmk.rulesets.v1.form_specs import (
+    DefaultValue,
+    DictElement,
+    Dictionary,
+    SingleChoice,
+    SingleChoiceElement,
 )
-from cmk.gui.valuespec import DropdownChoice
-from cmk.ruleset_matcher.definition import RuleGroup
+from cmk.rulesets.v1.rule_specs import AgentConfig, Topic
+
+_VALID_VALUES = frozenset(("inetd", "no_service"))
 
 
-def _valuespec_super_server_solaris() -> DropdownChoice[str]:
-    return DropdownChoice(
-        title=_("Checkmk agent network service (Solaris)"),
-        help=_(
+def migrate(value: object) -> Mapping[str, object]:
+    if isinstance(value, dict):
+        return value
+    if isinstance(value, str) and value in _VALID_VALUES:
+        return {"deployment": value}
+    raise ValueError(f"Unexpected value: {value!r}")
+
+
+def _parameter_form_super_server_solaris() -> Dictionary:
+    return Dictionary(
+        title=Title("Checkmk agent network service (Solaris)"),
+        help_text=Help(
             "The Checkmk agent does not listen on its own for incoming network connections."
             " By default, it makes use of so called super servers, which"
             " listen on the network and dispatch incoming requests to applications like"
@@ -26,18 +38,31 @@ def _valuespec_super_server_solaris() -> DropdownChoice[str]:
             " With this rule, you can optionally disable the service installation"
             " , e.g., if you connect to the agent via SSH.\n"
         ),
-        default_value="inetd",
-        choices=[
-            ("inetd", _("Install and activate inetd service")),
-            ("no_service", _("Don't install Checkmk service")),
-        ],
+        elements={
+            "deployment": DictElement(
+                required=True,
+                parameter_form=SingleChoice(
+                    elements=[
+                        SingleChoiceElement(
+                            name="inetd",
+                            title=Title("Install and activate inetd service"),
+                        ),
+                        SingleChoiceElement(
+                            name="no_service",
+                            title=Title("Don't install Checkmk service"),
+                        ),
+                    ],
+                    prefill=DefaultValue("inetd"),
+                ),
+            ),
+        },
+        migrate=migrate,
     )
 
 
-rulespec_registry.register(
-    HostRulespec(
-        group=RulespecGroupMonitoringAgentsLinuxUnixAgent,
-        name=RuleGroup.AgentConfig("super_server_solaris"),
-        valuespec=_valuespec_super_server_solaris,
-    )
+rule_spec_super_server_solaris = AgentConfig(
+    name="super_server_solaris",
+    title=Title("Checkmk agent network service (Solaris)"),
+    topic=Topic.LINUX,
+    parameter_form=_parameter_form_super_server_solaris,
 )
