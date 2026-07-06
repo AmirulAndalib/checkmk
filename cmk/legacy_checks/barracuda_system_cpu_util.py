b59f8129506b83de343368b2d1ae1d60fbb6c2d0
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-07-06 16:32:57 +0200

    Legacy check migration: barracuda_system_cpu_util I
    Change-Id: Id5e699082ecb39618571b51cbeb8927580c9db78

--- cmk/legacy_checks/barracuda_system_cpu_util.py
+++ cmk/legacy_checks/barracuda_system_cpu_util.py
@@ -3,45 +3,61 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-call"
-# mypy: disable-error-code="no-untyped-def"
+import time
+from collections.abc import Mapping
+from typing import Any
 
-
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import SNMPTree, StringTable
-from cmk.legacy_includes.cpu_util import check_cpu_util
+from cmk.agent_based.v2 import (
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    get_value_store,
+    Service,
+    SimpleSNMPSection,
+    SNMPTree,
+    StringTable,
+)
 from cmk.plugins.barracuda.lib import DETECT_BARRACUDA
-
-check_info = {}
+from cmk.plugins.lib.cpu_util import check_cpu_util
 
 # .1.3.6.1.4.1.20632.2.13 3
 
 # Suggested by customer
 
 
-def discover_barracuda_system_cpu_util(info):
-    yield None, {}
+def discover_barracuda_system_cpu_util(section: StringTable) -> DiscoveryResult:
+    yield Service()
 
 
-def check_barracuda_system_cpu_util(_no_item, params, info):
-    return check_cpu_util(int(info[0][0]), params)
+def check_barracuda_system_cpu_util(params: Mapping[str, Any], section: StringTable) -> CheckResult:
+    yield from check_cpu_util(
+        util=int(section[0][0]),
+        params=params,
+        value_store=get_value_store(),
+        this_time=time.time(),
+    )
 
 
 def parse_barracuda_system_cpu_util(string_table: StringTable) -> StringTable | None:
     return string_table or None
 
 
-check_info["barracuda_system_cpu_util"] = LegacyCheckDefinition(
+snmp_section_barracuda_system_cpu_util = SimpleSNMPSection(
     name="barracuda_system_cpu_util",
-    parse_function=parse_barracuda_system_cpu_util,
     detect=DETECT_BARRACUDA,
     # The barracuda spam firewall does not response or returns a timeout error
     # executing 'snmpwalk' on whole tables. But we can workaround here specifying
-    # all needed OIDs. Then we can use 'snmpget' and 'snmpwalk' on these single OIDs.,
+    # all needed OIDs. Then we can use 'snmpget' and 'snmpwalk' on these single OIDs.
     fetch=SNMPTree(
         base=".1.3.6.1.4.1.20632.2",
         oids=["13"],
     ),
+    parse_function=parse_barracuda_system_cpu_util,
+)
+
+
+check_plugin_barracuda_system_cpu_util = CheckPlugin(
+    name="barracuda_system_cpu_util",
     service_name="CPU utilization",
     discovery_function=discover_barracuda_system_cpu_util,
     check_function=check_barracuda_system_cpu_util,
