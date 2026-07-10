cd366fbcbe782235c39174d6c28846a41b5ac39f
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-07-10 16:05:02 +0200

    Legacy check migration: aws_elbv2_limits II
    Change-Id: I2c385b017ecead04afadfdde830cf579396fe310

--- packages/cmk-plugins/tests/cmk/plugins/aws/agent_based/test_aws_elbv2_limits.py
+++ packages/cmk-plugins/tests/cmk/plugins/aws/agent_based/test_aws_elbv2_limits.py
@@ -11,7 +11,7 @@ from typing import Any
 import pytest
 
 from cmk.agent_based.v2 import Metric, Result, Service, State, StringTable
-from cmk.legacy_checks.aws_elbv2_limits import (
+from cmk.plugins.aws.agent_based.aws_elbv2_limits import (
     check_aws_elbv2_limits,
     discover_aws_elbv2_limits,
 )
