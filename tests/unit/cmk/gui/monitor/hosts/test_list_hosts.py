d8aa7ff996b5257bddd9bbb7d8e349b384a7d3e3
Logan Connolly <logan.connolly@checkmk.com>
2026-07-13 15:17:16 +0200

    monitor(hosts): factor out factory and fixtures
    Change-Id: I4c47a5cc61ae47ac375b87fd5712c6b1c56f4e36

--- tests/unit/cmk/gui/monitor/hosts/test_list_hosts.py
+++ tests/unit/cmk/gui/monitor/hosts/test_list_hosts.py
@@ -2,43 +2,9 @@
 # Copyright (C) 2026 Checkmk GmbH - License: GNU General Public License v2
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
-
-from collections.abc import Sequence
-
-from polyfactory.factories import DataclassFactory
-
 from cmk.gui.monitor.hosts._api._list_hosts import _build_host_modes, _handle_list_hosts
-from cmk.gui.monitor.hosts._models import Host, HostFilter, HostSort
-from cmk.gui.monitor.hosts._repositories import HostRepository
 
-
-class HostFactory(DataclassFactory[Host]):
-    __check_model__ = False
-
-
-def get_fake_host_repository(*, n_hosts: int) -> HostRepository:
-    class HostFakeRepository:
-        def __init__(self) -> None:
-            self._hosts = [HostFactory.build() for _ in range(n_hosts)]
-
-        def fetch(
-            self,
-            *,
-            limit: int,
-            query: str,
-            sorters: Sequence[HostSort],
-            filters: HostFilter,
-        ) -> Sequence[Host]:
-            return self._hosts[:limit]
-
-        def count_total(self) -> int:
-            return len(self._hosts)
-
-        def count_matched(self, *, query: str, filters: HostFilter) -> int:
-            # Not implementing this as we don't need to test a fake implementation of this.
-            return self.count_total()
-
-    return HostFakeRepository()
+from .testlib import get_fake_host_repository, HostFactory
 
 
 def test_handle_list_hosts_limit_handling() -> None:
