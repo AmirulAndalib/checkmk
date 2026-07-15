f3b232131d644533edff61590f791bbaf392e5ea
Logan Connolly <logan.connolly@checkmk.com>
2026-07-15 10:34:33 +0200

    search: move protocol close to its consumer
    Change-Id: I49c6276a0a8993b8d30771b90c7c31d52e832215

--- cmk/gui/search/type_defs.py
+++ cmk/gui/search/type_defs.py
@@ -3,21 +3,10 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-from collections.abc import Callable, Iterable
+from collections.abc import Callable
 from typing import Protocol
 
-from cmk.shared_typing.unified_search import ProviderName, UnifiedSearchResultItem
-
 type VisibilityCheck = Callable[[str], bool]
-
-
-class SearchEngine(Protocol):
-    def search(
-        self,
-        query: str,
-        *,
-        provider: ProviderName,
-    ) -> Iterable[UnifiedSearchResultItem]: ...
 
 
 class SearchPermissionsHandler(Protocol):
--- cmk/gui/search/unified.py
+++ cmk/gui/search/unified.py
@@ -3,6 +3,9 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
+from collections.abc import Iterable
+from typing import Protocol
+
 from cmk.shared_typing.unified_search import (
     ProviderName,
     SortType,
@@ -12,7 +15,15 @@ from cmk.shared_typing.unified_search import (
 )
 
 from .sorting import get_sorter
-from .type_defs import SearchEngine
+
+
+class SearchEngine(Protocol):
+    def search(
+        self,
+        query: str,
+        *,
+        provider: ProviderName,
+    ) -> Iterable[UnifiedSearchResultItem]: ...
 
 
 class UnifiedSearch:
